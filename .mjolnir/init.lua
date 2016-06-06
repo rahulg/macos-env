2ab2c1781ea5a172b338da333125846767ab6c35
Rahul AG <r@hul.ag>
2016-06-06 13:34:35 +0800

    Drop c-a-Down and c-a-Up, rename c-a-F to c-a-Up.

--- .mjolnir/init.lua
+++ .mjolnir/init.lua
@@ -46,42 +46,6 @@ hotkey.bind(
 		local win = window.focusedwindow()
 		if win == nil then return end
 		local frm = win:frame()
-		local scr = win:screen()
-		local scr_out = scr:fullframe()
-		local scr_in = scr:frame()
-		frm.x = scr_out.w - scr_in.w
-		frm.y = scr_out.h - scr_in.h
-		frm.h = scr_in.h / 2
-		frm.w = scr_in.w
-		win:setframe(frm)
-	end
-)
-
-hotkey.bind(
-	{ "cmd", "alt" },
-	"Down",
-	function()
-		local win = window.focusedwindow()
-		if win == nil then return end
-		local frm = win:frame()
-		local scr = win:screen()
-		local scr_out = scr:fullframe()
-		local scr_in = scr:frame()
-		frm.x = scr_out.w - scr_in.w
-		frm.y = (scr_in.h / 2) + (scr_out.h - scr_in.h)
-		frm.h = scr_in.h / 2
-		frm.w = scr_in.w
-		win:setframe(frm)
-	end
-)
-
-hotkey.bind(
-	{ "cmd", "alt" },
-	"F",
-	function()
-		local win = window.focusedwindow()
-		if win == nil then return end
-		local frm = win:frame()
 		local scr_in = win:screen():frame()
 		win:setframe(scr_in)
 	end
